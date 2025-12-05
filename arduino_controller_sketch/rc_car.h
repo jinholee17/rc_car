8f12f033ab342c04a1a89b1cbb8422e4c792c390
cadenschroeder <caden@flexibly.ai>
2025-12-05 18:14:05 -0500

    full fsm + tests + steering clamps

--- arduino_controller_sketch/rc_car.h
+++ arduino_controller_sketch/rc_car.h
@@ -1,3 +1,7 @@
+// rc_car.h
+#ifndef RC_CAR_H
+#define RC_CAR_H
+
 typedef enum {
   s_IDLE = 0,
   s_MOVE = 1,
@@ -9,3 +13,5 @@ typedef struct {
   int turn;
   fsm_state state;
 } full_state;
+
+#endif
